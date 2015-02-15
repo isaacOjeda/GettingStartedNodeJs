1d05ae42f4a35ef53f540590fe812756bf09d233
Isaac Ojeda <isaacoq@gmail.com>
2015-02-14 18:16:34 -0700

    - Aplicación del post primera parte

---
+++ GettingStartedNodeJs/models/customerModel.ts
@@ -0,0 +1,29 @@
+﻿/*
+ * By Isaac O.Q.
+ */
+
+/*
+ * Representa un cliente
+ */
+export class CustomerModel{
+    /*
+     * Nombre del cliente
+     */
+    name: string;
+    /*
+     * Edad del cliente
+     */
+    age: number;
+    /*
+     * Email del cliente
+     */
+    email: string;
+    /*
+     * inicializa el modelo
+     */
+    constructor(name: string, age: number, email: string) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+}
---
+++ GettingStartedNodeJs/services/customerService.ts
@@ -0,0 +1,26 @@
+﻿/*
+ * By Isaac O.Q.
+ */
+
+import customer = require('../models/customerModel');
+
+export class CustomerService {
+    /*
+     * Array dummy de objetos CustomerModel
+     */
+    dummyRepository: customer.CustomerModel[];
+    /*
+     * Inicializa el array sin datos
+     */
+    constructor() {
+        this.dummyRepository = [];
+    }
+    /*
+     * Agrega un nuevo cliente
+     * @param newCustomer Nuevo cliente a agregar
+     */
+    add(newCustomer: customer.CustomerModel): void {
+        this.dummyRepository.push(newCustomer);
+    }
+
+}
